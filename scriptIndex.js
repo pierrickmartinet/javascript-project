58ab8aa3e4a50fd33965db2f4c8e26d3d70ef958
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-03-26 09:41:06 +0100

    Feat: feed de la page d'accueil #2

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
     console.log('Je peux maintenant écrire du code jQuery');
 
-$.ajax({
-    // L'URL de la requête
-    
-})
+    // Appelle et traitement API par requête Ajax
+
+    $.getJSON("http://api.icndb.com/jokes/random/20")
+
+        // En cas de succés de la requête Ajax
+        .done(function (response) {
+            let index = 0;
+            // Récupère le contenu (un objet) de la requête et le place dans une variable data
+            let data = response;
+            console.log(data);
+
+            while (index < data.value.length){
+                // Récupère dans l'objet data la propriété joke, la place dans une div et place cette div dans la div "feed"
+                $('<div class="blagues"></div>').append(data.value[index].joke).prependTo('#feed');
+                index += 1;
+            }
+
+        })
+
+        // En cas d'échec de la requête Ajax
+        .fail(function(error){
+            alert("La requête s'est terminée en échec. Infos:" + JSON.stringify(error));
+        })
+
+        // Toujours exécuté que se soit succés ou échec
+        .always(function(){
+            //alert("Requête effectuée");
+        });
 
 });
