e85fddeb1875f0e2e79c560e576d8c6789c83a56
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-04-01 14:33:50 +0200

    Feat: Comptage points jeu Tic Tac Toe

--- scriptJeu.js
+++ scriptJeu.js
@@ -1,16 +1,42 @@
 $(document).ready(function () {
     console.log('Je peux maintenant écrire du code jQuery');
 
+    // Menu dropDown
+
+    // Séléction de la div de la classe dropdown 
+    let dropdown = document.querySelector('.dropdown');
+
+
+    // Au clic si fermé: ouvre sinon ferme
+    dropdown.addEventListener('click', (e) => {
+
+        if (dropdown.classList.contains('closed')) {
+            dropdown.classList.remove('closed');
+        } else {
+            dropdown.classList.add('closed');
+        }
+
+    })
+
+
     // On charge les informations utiles
 
     // Récupération de la balise h2
     const statut = document.querySelector("#message");
+    // Récupération de la balise p portant l'id pointX
+    const pointX = document.querySelector("#pointX");
+    // Récupération de la balise p portant l'id pointO
+    const pointO = document.querySelector("#pointO");
     // Lorsqu'on joue la valeur de jeuActif est à true, sinon false
     let jeuActif = true;
     // Si le joueur "X" joue la valeur est à "X", si le joueur "O" joue la valeur est à "O"
     let joueurActif = "X";
     // Létat du jeu sur la grille rempli avec "X" ou "O" au fur et à mesure
     let etatJeu = ["", "", "", "", "", "", "", "", ""];
+
+    // initialisation des variables de points à 0
+    let nombrePointX = 0;
+    let nombrePointO = 0;
 
     // Liste les possibilités de victoire (3 lignes, 3 colonnes, 2 diagonales)
     const conditionsVictoire = [
@@ -30,14 +56,21 @@ $(document).ready(function () {
         [0, 4, 8],
         // Deuxième diagonale
         [2, 4, 6],
-    ]
+    ];
 
     // Messages
-    const gagne = () => `le joueur ${joueurActif} a  gagné`;
+    const gagne = () => `le joueur ${joueurActif} a gagné`;
     const egalite = () => "Egalité";
     const tourJoueur = () => `C'est au tour du joueur ${joueurActif}`;
+    let totalPointX = () => `Points joueur X : ${nombrePointX}`;
+    let totalPointO = () => `Points joueur O : ${nombrePointO}`;
 
+    // Insère dans le DOM la finction tourJoueur "C'est au tou du joueur "joueur actif"
     statut.innerHTML = tourJoueur();
+    // Insère dans le DOM nombre de point joueur X (0 par défaut)
+    pointX.innerHTML = totalPointX();
+    // Insère dans le DOM nombre de point joueur O (0 par défaut)
+    pointO.innerHTML = totalPointO();
 
 
     // Séléction de chacune des cases, pour chaque case un Listener qui écoute click et éxécute la fonction gestionClicCase()
@@ -46,6 +79,8 @@ $(document).ready(function () {
     // Séléction du bouton recommencer qui écoute le click et éxécute la fonction recommencer()
     document.querySelector("#recommencer").addEventListener("click", recommencer);
 
+    // Séléction du bouton reinitialiser qui écoute le click et éxécute la fonction reinitialiserScore
+    document.querySelector("#reinitialiserScore").addEventListener("click", reinitialiserScore);
 
     //Fonctions
 
@@ -69,43 +104,57 @@ $(document).ready(function () {
 
 
     // fonction verifGagne qui vérifie si 3 "X" ou 3 "O" sont alignés (si un joueur à gagné)
-    function verifGagne(){
+    function verifGagne() {
 
         // Par défaut tourGagnant à la valeur "Faux"
-        let tourGagnant = false
+        let tourGagnant = false;
 
         // Vérification sur chacune des conditions de victoire (tableau consitionsVictoire) comparatif avec le tableau etatJeu
-        for (let conditionVictoire of conditionsVictoire){
+        for (let conditionVictoire of conditionsVictoire) {
+
 
             let val1 = etatJeu[conditionVictoire[0]];
             let val2 = etatJeu[conditionVictoire[1]];
             let val3 = etatJeu[conditionVictoire[2]];
 
             // Si l'une des valeurs est vide, je peux sortir de ma vérification c'est que le joueur n'a pas gagné sur cette occurence
-            if ( (val1 == "") || (val2 == "") || (val3 == "") ){
-                continue
+            if ((val1 == "") || (val2 == "") || (val3 == "")) {
+                continue;
             }
             // Si les 3 valeurs sont remplis je vérifie si c'est bien le même joueur
-            if ( (val1 == val2) && (val2 == val3) ){
-                tourGagnant = true
-                break
+            if ((val1 == val2) && (val2 == val3)) {
+                tourGagnant = true;
+
+                if (joueurActif == "X") {
+                    // Ajout d'un point au joueur X
+                    nombrePointX = nombrePointX + 1;
+                    pointX.innerHTML = totalPointX();
+                    break;
+
+                } else {
+                    // Ajout d'un point au joueur O
+                    nombrePointO = nombrePointO + 1;
+                    pointO.innerHTML = totalPointO();
+                    break;
+                }
+
             }
         }
 
         // Je vérifie si tour gagnant est égal à true, si oui je change le statut de mon h2 à gagné et j'arrète le jeu et je quitte
-        if(tourGagnant){
+        if (tourGagnant) {
             statut.innerHTML = gagne();
-            jeuActif = false
-            return
+            jeuActif = false;
+            return;
         }
 
         // Je vérifie si le tableau n'est pas plein, si il est plein alors égalité et j'arrête le jeu et je quitte
 
         // Si le tableau etatJeu n'inclue pas de vide (!etatJeu.include = "")
-        if(!etatJeu.includes("")){
+        if (!etatJeu.includes("")) {
             statut.innerHTML = egalite();
-            jeuActif = false
-            return
+            jeuActif = false;
+            return;
         }
 
         // Si pas de gagnant, ni d'égalité alors le jeu continue et je change de joueur
@@ -115,7 +164,7 @@ $(document).ready(function () {
     }
 
     // fonction recommencer qui remet le jeu à zéro
-    function recommencer(){
+    function recommencer() {
         // Remettre par défaut le joueur actif en "X"
         joueurActif = "X";
         // Jeu actif en true
@@ -126,6 +175,15 @@ $(document).ready(function () {
         statut.innerHTML = tourJoueur();
         // Remise à zéro des cases du jeu
         document.querySelectorAll(".case").forEach(cell => cell.innerHTML = "");
+    }
+
+    // Fonction qui réinitialise les scores à 0
+
+    function reinitialiserScore() {
+        nombrePointX = 0;
+        pointX.innerHTML = totalPointX();
+        nombrePointO = 0;
+        pointO.innerHTML = totalPointO();
     }
 
 
